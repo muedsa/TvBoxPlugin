6e4f6af6865d531b8e9140036f74d94f76702cce
MUEDSA <7676275+muedsa@users.noreply.github.com>
2024-10-11 11:08:40 +0800

    refactor: 在build.gradle.kts中控制依赖版本

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.serialization)
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 android {
@@ -35,10 +35,17 @@ android {
 
 dependencies {
     // 提供以下依赖的API给TvBox和插件使用
-    api(libs.jsoup)
-    api(libs.ktx.serialization)
-    api(libs.retrofit2)
-    api(libs.retrofit2.ktx.serialization)
-    api(libs.okhttp3.logging)
-    api(libs.timber)
+    val jsoupVersion = "1.18.1"
+    val ktxJsonVersion = "1.7.3"
+    val retrofitVersion = "2.11.0"
+    val retrofitKtxSerialization = "1.0.0"
+    val okhttp3LoggingVersion = "4.12.0"
+    val timberVersion = "5.0.1"
+
+    api("org.jsoup:jsoup:$jsoupVersion")
+    api("org.jetbrains.kotlinx:kotlinx-serialization-json:$ktxJsonVersion")
+    api("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    api("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:$retrofitKtxSerialization")
+    api("com.squareup.okhttp3:logging-interceptor:$okhttp3LoggingVersion")
+    api("com.jakewharton.timber:timber:$timberVersion")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
+    id("com.android.library") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
 }
