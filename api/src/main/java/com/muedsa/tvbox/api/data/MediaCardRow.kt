170ba237582cf0a3eb406503be893e4667b2d8fa
MUEDSA <7676275+muedsa@users.noreply.github.com>
2024-10-12 17:17:26 +0800

    fix: 错误拼写的字段 cardWith -> cardWidth

--- api/src/main/java/com/muedsa/tvbox/api/data/MediaCardRow.kt
+++ api/src/main/java/com/muedsa/tvbox/api/data/MediaCardRow.kt
@@ -4,7 +4,7 @@ data class MediaCardRow(
     // val key: String,
     val title: String,
     val list: List<MediaCard>,
-    val cardWith: Int = 200, // dp
+    val cardWidth: Int = 200, // dp
     val cardHeight: Int = 283, // dp
     val cardType: MediaCardType = MediaCardType.STANDARD
 )
