1f1c8dae83c62695ea3d9ea8453fa74d30962f17
MUEDSA <7676275+muedsa@users.noreply.github.com>
2024-10-14 11:43:37 +0800

    feat: 媒体详情添加子标题和描述

--- api/src/main/java/com/muedsa/tvbox/api/data/MediaDetail.kt
+++ api/src/main/java/com/muedsa/tvbox/api/data/MediaDetail.kt
@@ -4,6 +4,8 @@ open class MediaDetail(
     id: String,
     title: String,
     detailUrl: String,
+    val subTitle: String?,
+    val description: String?,
     val backgroundImageUrl: String,
     // val backgroundImageHttpHeaders: Map<String, String>? = null,
     val playSourceList: List<MediaPlaySource>,
