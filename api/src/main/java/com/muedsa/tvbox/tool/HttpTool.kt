ed67c0201f47017d2e487c307d205f7e6fe9080c
MUEDSA <7676275+muedsa@users.noreply.github.com>
2024-10-25 15:40:37 +0800

    feat: HttpTool增加 cookJar参数

--- api/src/main/java/com/muedsa/tvbox/tool/HttpTool.kt
+++ api/src/main/java/com/muedsa/tvbox/tool/HttpTool.kt
@@ -1,6 +1,7 @@
 package com.muedsa.tvbox.tool
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import okhttp3.CookieJar
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -9,8 +10,9 @@ import retrofit2.Retrofit
 const val ChromeUserAgent =
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
 
-fun createOkHttpClient(debug: Boolean = false): OkHttpClient =
+fun createOkHttpClient(debug: Boolean = false, cookieJar: CookieJar = CookieJar.NO_COOKIES): OkHttpClient =
     OkHttpClient.Builder()
+        .cookieJar(cookieJar)
         .apply {
             if (debug) {
                 addInterceptor(HttpLoggingInterceptor()
@@ -19,11 +21,16 @@ fun createOkHttpClient(debug: Boolean = false): OkHttpClient =
         }
         .build()
 
-fun <T> createJsonRetrofit(baseUrl: String, service: Class<T>, debug: Boolean = false): T {
+fun <T> createJsonRetrofit(
+    baseUrl: String,
+    service: Class<T>,
+    debug: Boolean = false,
+    cookieJar: CookieJar = CookieJar.NO_COOKIES
+): T {
     return Retrofit.Builder()
         .baseUrl(baseUrl)
         .addConverterFactory(LenientJson.asConverterFactory("application/json".toMediaType()))
-        .client(createOkHttpClient(debug))
+        .client(createOkHttpClient(debug = debug, cookieJar = cookieJar))
         .build()
         .create(service)
 }
