32e288c98bbd0f3647443ce46fdba1420f1ea83f
MUEDSA <7676275+muedsa@users.noreply.github.com>
2026-04-15 17:51:06 +0800

    feat: MediaMergingHttpSource 也增加视频自动跳过指定片段的参数

--- api/src/main/java/com/muedsa/tvbox/api/data/MediaMergingHttpSource.kt
+++ api/src/main/java/com/muedsa/tvbox/api/data/MediaMergingHttpSource.kt
@@ -3,7 +3,15 @@ package com.muedsa.tvbox.api.data
 class MediaMergingHttpSource(
     val urls: List<String>,
     httpHeaders: Map<String, String>? = null,
+    skipSegments: List<Pair<Long, Long>>? = null,
 ) : MediaHttpSource (
     url = urls[0],
-    httpHeaders = httpHeaders
-)
+    httpHeaders = httpHeaders,
+    skipSegments = skipSegments,
+) {
+    constructor(urls: List<String>, httpHeaders: Map<String, String>?) : this(
+        urls = urls,
+        httpHeaders = httpHeaders,
+        skipSegments = null,
+    )
+}
